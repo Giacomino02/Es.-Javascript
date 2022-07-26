dfe08b13ba1f3defe6d6aa0d09ccf2ace769f5d7
Giacomo Granducci <106163716+Giacomino02@users.noreply.github.com>
2022-07-26 19:35:44 +0200

    esercizio 3 completato

---
+++ es-03/exercise-3.js
@@ -0,0 +1,14 @@
+const students = ['Paul', 'George', 'Lucas'];
+
+function addStudent(student) {
+  // ...
+}
+
+
+students.push('Marco');
+// oppure
+// students[students.length]= 'Marco';
+
+addStudent('Marco');
+console.log(students);
+

706348643f858b0d7a896a7aadaa3721f257cd07
Giacomo Granducci <106163716+Giacomino02@users.noreply.github.com>
2022-09-06 21:37:54 +0200

    Esercizio 55 corretto aggiornato

--- es-55/exercise-55.js
+++ es-55/exercise-55.js
@@ -1,6 +1,6 @@
 let numberStore = [0, 1, 2];
-let newNumber = [...numberStore, 3];
+let newNumber = 3;
 // numberStore.push(newNumber);
+numberStore = [...numberStore, newNumber]
 
-
-console.log(newNumber);
+console.log(numberStore);
